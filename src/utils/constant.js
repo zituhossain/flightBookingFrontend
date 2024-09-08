8f91af4b9b05a9745f0e33cc51ead75e0fd133ef
zituhossain5 <zituhossain@gmail.com>
2024-09-08 22:19:18 +0600

    add icon

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,7 +2,7 @@ export const USER_API_END_POINT =
   "https://flightbookingbackend-ql1d.onrender.com/api/user";
 
 export const FLIGHT_API_END_POINT =
-  "https://flight-booking-backend-six.vercel.app/api/flight";
+  "https://flightbookingbackend-ql1d.onrender.com/api/flight";
 
 export const BOOKING_API_END_POINT =
-  "https://flight-booking-backend-six.vercel.app/api/booking";
+  "https://flightbookingbackend-ql1d.onrender.com/api/booking";
