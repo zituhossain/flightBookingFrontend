a20b0e89cdb104d1aa55bea0e1eea3f1fb2705f1
zituhossain5 <zituhossain@gmail.com>
2024-09-08 22:17:15 +0600

    update constant

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
 export const USER_API_END_POINT =
-  "https://flight-booking-backend-six.vercel.app/api/user";
+  "https://flightbookingbackend-ql1d.onrender.com/api/user";
 
 export const FLIGHT_API_END_POINT =
   "https://flight-booking-backend-six.vercel.app/api/flight";
